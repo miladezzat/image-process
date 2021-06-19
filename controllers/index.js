7115d93564de7773fb7e83ee9f91b7d0276a2b03
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-06-19 23:30:13 +0200

    setup website

---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const indexService = require('../services');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/data-base64', async (req, res) => {
+  const image = await indexService.processImageAsBase64(req.query);
+
+  res.send({ image: `data:image/png;base64, ${image}` });
+});
+
+router.get('/url', async (req, res) => {
+  const image = await indexService.processImageAsStream(req.query);
+  image.pipe(res);
+});
+
+module.exports = router;
---
+++ services/index.js
@@ -0,0 +1,55 @@
+const axios = require('axios').default;
+const sharp = require('sharp');
+const streamToBuffer = require('stream-to-buffer');
+const util = require('util');
+
+const streamToBufferPromise = util.promisify(streamToBuffer);
+
+const readImage = async (url) => axios
+  .get(url, {
+    responseType: 'stream',
+  });
+
+const sharpImage = async (args) => {
+  const {
+    imageUrl,
+    toBuffer = false,
+    imageFormat = 'png',
+    imageWidth = null,
+    imageHeight = null,
+  } = args;
+  const resizeOptions = {};
+
+  if (imageWidth) {
+    resizeOptions.width = +imageWidth;
+  }
+
+  if (imageHeight) {
+    resizeOptions.height = +imageHeight;
+  }
+
+  // contentType
+  const { data } = await readImage(imageUrl);
+
+  const buffer = await streamToBufferPromise(data);
+
+  if (toBuffer) {
+    return sharp(buffer)
+      .toFormat(imageFormat)
+      .resize(resizeOptions)
+      .toBuffer();
+  }
+
+  return sharp(buffer)
+    .resize(resizeOptions)
+    .toFormat(imageFormat);
+};
+
+const processImageAsBase64 = async (args) => {
+  const image = await sharpImage({ ...args, toBuffer: true });
+  return image.toString('base64');
+};
+
+const processImageAsStream = async (args) => sharpImage(args);
+
+module.exports = { processImageAsBase64, processImageAsStream };
